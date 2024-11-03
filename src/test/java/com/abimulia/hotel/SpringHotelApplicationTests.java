5c60910dc026831e3d584ac900f2dd4a1a3a2453
abimulia <abimulia@gmail.com>
2024-11-03 21:24:25 +0700

    initial commit

---
+++ src/test/java/com/abimulia/hotel/SpringHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abimulia.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
